a5b8bde39e15cb3d4315f09eb823b993bb589ffe
ryze1001 <10440661@qq.com>
2020-10-09 16:11:48 +0800

    Add files via upload

---
+++ slimming/vggprune.py
@@ -0,0 +1,278 @@
+import os
+import argparse
+import numpy as np
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+from torchvision import datasets, transforms
+from models import *
+import torch.optim as optim
+import torch.nn.functional as F
+
+# Prune settings
+parser = argparse.ArgumentParser(description='PyTorch Slimming CIFAR prune')
+parser.add_argument('--dataset', type=str, default='cifar100',
+                    help='training dataset (default: cifar10)')
+parser.add_argument('--test-batch-size', type=int, default=256, metavar='N',
+                    help='input batch size for testing (default: 256)')
+parser.add_argument('--no-cuda', action='store_true', default=False,
+                    help='disables CUDA training')
+parser.add_argument('--depth', type=int, default=19,
+                    help='depth of the vgg')
+parser.add_argument('--percent', type=float, default=0.35,
+                    help='scale sparse rate (default: 0.5)')
+parser.add_argument('--model', default='', type=str, metavar='PATH',
+                    help='path to the model (default: none)')
+parser.add_argument('--save', default='', type=str, metavar='PATH',
+                    help='path to save pruned model (default: none)')
+parser.add_argument('--batch-size', type=int, default=256, metavar='N',
+                    help='input batch size for training (default: 64)')
+parser.add_argument('--epochs', type=int, default=5, metavar='N',
+                    help='number of epochs to train (default: 160)')
+parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
+                    help='manual epoch number (useful on restarts)')
+parser.add_argument('--lr', type=float, default=0.1, metavar='LR',
+                    help='learning rate (default: 0.1)')
+parser.add_argument('--momentum', type=float, default=0.9, metavar='M',
+                    help='SGD momentum (default: 0.9)')
+parser.add_argument('--weight-decay', '--wd', default=1e-4, type=float,
+                    metavar='W', help='weight decay (default: 1e-4)')
+parser.add_argument('--log-interval', type=int, default=100, metavar='N',
+                    help='how many batches to wait before logging training status')
+args = parser.parse_args()
+args.cuda = not args.no_cuda and torch.cuda.is_available()
+
+if not os.path.exists(args.save):
+    os.makedirs(args.save)
+
+model = vgg(dataset=args.dataset, depth=args.depth)
+if args.cuda:
+    model.cuda()
+
+if args.model:
+    if os.path.isfile(args.model):
+        print("=> loading checkpoint '{}'".format(args.model))
+        checkpoint = torch.load(args.model)
+        args.start_epoch = checkpoint['epoch']
+        best_prec1 = checkpoint['best_prec1']
+        model.load_state_dict(checkpoint['state_dict'])
+        print("=> loaded checkpoint '{}' (epoch {}) Prec1: {:f}"
+              .format(args.model, checkpoint['epoch'], best_prec1))
+    else:
+        print("=> no checkpoint found at '{}'".format(args.resume))
+
+print(model)
+total = 0
+for m in model.modules():
+    if isinstance(m, nn.BatchNorm2d):
+        total += m.weight.data.shape[0]
+
+bn = torch.zeros(total)
+index = 0
+for m in model.modules():
+    if isinstance(m, nn.BatchNorm2d):
+        size = m.weight.data.shape[0]
+        bn[index:(index+size)] = m.weight.data.abs().clone()
+        index += size
+
+y, i = torch.sort(bn)
+thre_index = int(total * args.percent)
+thre = y[thre_index]
+
+pruned = 0
+cfg = []
+cfg_mask = []
+for k, m in enumerate(model.modules()):
+    if isinstance(m, nn.BatchNorm2d):
+        weight_copy = m.weight.data.abs().clone()
+        weight_copy = weight_copy.cpu()
+        mask = weight_copy.gt(thre).float().cuda()
+        weight_copy = weight_copy.cuda()
+        pruned = pruned + mask.shape[0] - torch.sum(mask)
+        m.weight.data.mul_(mask)
+        m.bias.data.mul_(mask)
+        if int(torch.sum(mask))  != 0:
+            cfg.append(int(torch.sum(mask)))
+            cfg_mask.append(mask.clone())
+        print('layer index: {:d} \t total channel: {:d} \t remaining channel: {:d}'.
+            format(k, mask.shape[0], int(torch.sum(mask))))
+    elif isinstance(m, nn.MaxPool2d):
+        cfg.append('M')
+
+pruned_ratio = pruned/total
+
+print('Pre-processing Successful!')
+
+# simple test model after Pre-processing prune (simple set BN scales to zeros)
+def test(model):
+    kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
+    if args.dataset == 'cifar10':
+        test_loader = torch.utils.data.DataLoader(
+            datasets.CIFAR10('../../data', train=False, transform=transforms.Compose([
+                transforms.ToTensor(),
+                transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])),
+            batch_size=args.test_batch_size, shuffle=True, **kwargs)
+    elif args.dataset == 'cifar100':
+        test_loader = torch.utils.data.DataLoader(
+            datasets.CIFAR100('../../data', train=False, transform=transforms.Compose([
+                transforms.ToTensor(),
+                transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])),
+            batch_size=args.test_batch_size, shuffle=True, **kwargs)
+    else:
+        raise ValueError("No valid dataset is given.")
+    model.eval()
+    correct = 0
+    for data, target in test_loader:
+        if args.cuda:
+            data, target = data.cuda(), target.cuda()
+        data, target = Variable(data, volatile=True), Variable(target)
+        output = model(data)
+        pred = output.data.max(1, keepdim=True)[1] # get the index of the max log-probability
+        correct += pred.eq(target.data.view_as(pred)).cpu().sum()
+
+    print('\nTest set: Accuracy: {}/{} ({:.1f}%)\n'.format(
+        correct, len(test_loader.dataset), 100. * correct / len(test_loader.dataset)))
+    return correct / float(len(test_loader.dataset))
+
+acc = test(model)
+
+# Make real prune
+print(cfg)
+newmodel = vgg(dataset=args.dataset, cfg=cfg)
+if args.cuda:
+    newmodel.cuda()
+
+num_parameters = sum([param.nelement() for param in newmodel.parameters()])
+savepath = os.path.join(args.save, "prune.txt")
+with open(savepath, "w") as fp:
+    fp.write("Configuration: \n"+str(cfg)+"\n")
+    fp.write("Number of parameters: \n"+str(num_parameters)+"\n")
+    fp.write("Test accuracy: \n"+str(acc))
+
+layer_id_in_cfg = 0
+start_mask = torch.ones(3)
+end_mask = cfg_mask[layer_id_in_cfg]
+for [m0, m1] in zip(model.modules(), newmodel.modules()):
+    if isinstance(m0, nn.BatchNorm2d):
+        idx1 = np.squeeze(np.argwhere(np.asarray(end_mask.cpu().numpy())))
+        if idx1.size == 1:
+            idx1 = np.resize(idx1,(1,))
+        m1.weight.data = m0.weight.data[idx1.tolist()].clone()
+        m1.bias.data = m0.bias.data[idx1.tolist()].clone()
+        m1.running_mean = m0.running_mean[idx1.tolist()].clone()
+        m1.running_var = m0.running_var[idx1.tolist()].clone()
+        layer_id_in_cfg += 1
+        start_mask = end_mask.clone()
+        if layer_id_in_cfg < len(cfg_mask):  # do not change in Final FC
+            end_mask = cfg_mask[layer_id_in_cfg]
+    elif isinstance(m0, nn.Conv2d):
+        idx0 = np.squeeze(np.argwhere(np.asarray(start_mask.cpu().numpy())))
+        idx1 = np.squeeze(np.argwhere(np.asarray(end_mask.cpu().numpy())))
+        print('In shape: {:d}, Out shape {:d}.'.format(idx0.size, idx1.size))
+        if idx0.size == 1:
+            idx0 = np.resize(idx0, (1,))
+        if idx1.size == 1:
+            idx1 = np.resize(idx1, (1,))
+        w1 = m0.weight.data[:, idx0.tolist(), :, :].clone()
+        w1 = w1[idx1.tolist(), :, :, :].clone()
+        m1.weight.data = w1.clone()
+    elif isinstance(m0, nn.Linear):
+        idx0 = np.squeeze(np.argwhere(np.asarray(start_mask.cpu().numpy())))
+        if idx0.size == 1:
+            idx0 = np.resize(idx0, (1,))
+        m1.weight.data = m0.weight.data[:, idx0].clone()
+        m1.bias.data = m0.bias.data.clone()
+
+torch.save({'cfg': cfg, 'state_dict': newmodel.state_dict()}, os.path.join(args.save, 'pruned.pth.tar'))
+
+print(newmodel)
+model = newmodel
+test(model)
+
+
+
+kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
+if args.dataset == 'cifar10':
+    train_loader = torch.utils.data.DataLoader(
+        datasets.CIFAR10('../../data', train=True, download=True,
+                       transform=transforms.Compose([
+                           transforms.Pad(4),
+                           transforms.RandomCrop(32),
+                           transforms.RandomHorizontalFlip(),
+                           transforms.ToTensor(),
+                           transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+                       ])),
+        batch_size=args.batch_size, shuffle=True, **kwargs)
+    test_loader = torch.utils.data.DataLoader(
+        datasets.CIFAR10('../../data', train=False, transform=transforms.Compose([
+                           transforms.ToTensor(),
+                           transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+                       ])),
+        batch_size=args.test_batch_size, shuffle=True, **kwargs)
+else:
+    train_loader = torch.utils.data.DataLoader(
+        datasets.CIFAR100('../../data', train=True, download=True,
+                       transform=transforms.Compose([
+                           transforms.Pad(4),
+                           transforms.RandomCrop(32),
+                           transforms.RandomHorizontalFlip(),
+                           transforms.ToTensor(),
+                           transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+                       ])),
+        batch_size=args.batch_size, shuffle=True, **kwargs)
+    test_loader = torch.utils.data.DataLoader(
+        datasets.CIFAR100('../../data', train=False, transform=transforms.Compose([
+                           transforms.ToTensor(),
+                           transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+                       ])),
+        batch_size=args.test_batch_size, shuffle=True, **kwargs)
+
+optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum, weight_decay=args.weight_decay)
+
+def train(epoch):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        if args.cuda:
+            data, target = data.cuda(), target.cuda()
+        data, target = Variable(data), Variable(target)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = F.cross_entropy(output, target)
+        pred = output.data.max(1, keepdim=True)[1]
+        loss.backward()
+        optimizer.step()
+        if batch_idx % args.log_interval == 0:
+            print('Train Epoch: {} [{}/{} ({:.1f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.item()))
+
+def test():
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            if args.cuda:
+                data, target = data.cuda(), target.cuda()
+            # data, target = Variable(data, volatile=True), Variable(target)
+            output = model(data)
+            test_loss += F.cross_entropy(output, target).item()* data.size(0) # sum up batch loss
+            pred = output.data.max(1, keepdim=True)[1] # get the index of the max log-probability
+            correct += pred.eq(target.data.view_as(pred)).cpu().sum()
+
+    test_loss /= len(test_loader.dataset)
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.1f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+    return correct / float(len(test_loader.dataset))
+
+
+best_prec1 = 0.
+for epoch in range(0, args.epochs):
+    if epoch in [args.epochs*0.5, args.epochs*0.75]:
+        for param_group in optimizer.param_groups:
+            param_group['lr'] *= 0.1
+    train(epoch)
+    prec1 = test()
+    is_best = prec1 > best_prec1
+    best_prec1 = max(prec1, best_prec1)
